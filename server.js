0d54d187c637fa432a943f1a84bc68aac71f95c7
ennald.silva <ennald.silva@gmail.com>
2026-01-01 04:49:40 -0300

    Primeiro Teste

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+
 const http = require('http');
 const hostname = '0.0.0.0';
 const port = 3000; // Porta interna do container
@@ -5,7 +7,7 @@ const port = 3000; // Porta interna do container
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-  res.end('Ola Mundo - DevOps 2025 \n');
+  res.end('Ola Mundo - DevOps 2025 - TESTE \n');
 });
 
 server.listen(port, hostname, () => {
